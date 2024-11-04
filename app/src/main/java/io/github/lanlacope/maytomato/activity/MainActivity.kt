6fb504172aafbc76f57cbe5ee30c1bad781be744
lanlacope <pokeuni150@gmail.com>
2024-11-04 14:11:18 +0900

    activityを整理

--- app/src/main/java/io/github/lanlacope/maytomato/activity/MainActivity.kt
+++ app/src/main/java/io/github/lanlacope/maytomato/activity/MainActivity.kt
@@ -1,4 +1,4 @@
-package io.github.lanlacope.maytomato
+package io.github.lanlacope.maytomato.activity
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
@@ -10,7 +10,6 @@ import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import io.github.lanlacope.maytomato.ui.theme.MaytomatoTheme
 
 class MainActivity : ComponentActivity() {
@@ -31,17 +30,9 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
+private fun Greeting(name: String, modifier: Modifier = Modifier) {
     Text(
         text = "Hello $name!",
         modifier = modifier
     )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    MaytomatoTheme {
-        Greeting("Android")
-    }
 }
---
+++ app/src/main/java/io/github/lanlacope/maytomato/activity/SettingActivity.kt
@@ -0,0 +1,46 @@
+package io.github.lanlacope.maytomato.activity
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import io.github.lanlacope.maytomato.ui.theme.MaytomatoTheme
+
+class SettingActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            MaytomatoTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+private fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+private fun GreetingPreview() {
+    MaytomatoTheme {
+        Greeting("Android")
+    }
+}
