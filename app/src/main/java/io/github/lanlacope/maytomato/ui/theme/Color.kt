9047a19f8ef8e0524c9590dd0f6ee2f56a0fbb0c
lanlacope <pokeuni150@gmail.com>
2025-01-11 00:05:14 +0900

    テーマの処理を修正

--- app/src/main/java/io/github/lanlacope/maytomato/ui/theme/Color.kt
+++ app/src/main/java/io/github/lanlacope/maytomato/ui/theme/Color.kt
@@ -2,12 +2,16 @@ package io.github.lanlacope.maytomato.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
 
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
+val RedDark = Color(0xFFFF7F7F)
+val RedLight = Color(0xFFFFD1D1)
 
-val Clear = Color(0x00000000)
+val PaupleDark = Color(0xFF7F7FFF)
+val PaupleLight = Color(0xFFD1D1FF)
+
+val BlueDark = Color(0xFF7FBFFF)
+val BlueLight = Color(0xFFA8D3FF)
+
+val Gray = Color(0xFFC4C4C4)
+val White = Color(0xFFF9F9F9)
+val Black = Color(0xFF7F7F7F)
