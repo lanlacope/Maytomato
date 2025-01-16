c9dceb391a22c7488b03d4c2513e42c1de7ea247
lanlacope <pokeuni150@gmail.com>
2025-01-17 02:11:35 +0900

    未使用インポートの削除

--- app/src/main/java/io/github/lanlacope/maytomato/activity/component/SettingAbout.kt
+++ app/src/main/java/io/github/lanlacope/maytomato/activity/component/SettingAbout.kt
@@ -1,6 +1,5 @@
 package io.github.lanlacope.maytomato.activity.component
 
-import android.app.Activity
 import android.content.Intent
 import android.content.pm.PackageManager
 import android.net.Uri
