7e1a3c5e3fdef4099abf35f9c58682f88f70d448
lanlacope <pokeuni150@gmail.com>
2025-03-15 17:34:59 +0900

    動作していなかったので修正

--- app/src/main/java/io/github/lanlacope/maytomato/clazz/String.kt
+++ app/src/main/java/io/github/lanlacope/maytomato/clazz/String.kt
@@ -3,8 +3,8 @@ package io.github.lanlacope.maytomato.clazz
 fun String.toDecimal(): String {
     return buildString {
         var index = 0
-        while (index < length) {
-            val codePoint = codePointAt(index)
+        while (index < this@toDecimal.length) {
+            val codePoint = this@toDecimal.codePointAt(index)
             appendDecimal(codePoint)
             // サロゲートペアの場合は下位文字の分も進める
             index += if (Character.isSupplementaryCodePoint(codePoint)) 2 else 1
@@ -15,8 +15,8 @@ fun String.toDecimal(): String {
 fun String.toHexadecimal(): String {
     return buildString {
         var index = 0
-        while (index < length) {
-            val codePoint = codePointAt(index)
+        while (index < this@toHexadecimal.length) {
+            val codePoint = this@toHexadecimal.codePointAt(index)
             appendHexadecimal(codePoint)
             // サロゲートペアの場合は下位文字の分も進める
             index += if (Character.isSupplementaryCodePoint(codePoint)) 2 else 1
@@ -27,8 +27,8 @@ fun String.toHexadecimal(): String {
 fun String.toEntity(): String {
     return buildString {
         var index = 0
-        while (index < length) {
-            val codePoint = codePointAt(index)
+        while (index < this@toEntity.length) {
+            val codePoint = this@toEntity.codePointAt(index)
             appendEntity(codePoint)
             // サロゲートペアの場合は下位文字の分も進める
             index += if (Character.isSupplementaryCodePoint(codePoint)) 2 else 1
