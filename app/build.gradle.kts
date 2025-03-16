4783c2f237d8327f1956a0dd08b59a5cc5de7eae
lanlacope <pokeuni150@gmail.com>
2025-03-16 17:56:24 +0900

    release 2.2.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "io.github.lanlacope.maytomato"
         minSdk = 21
         targetSdk = 35
-        versionCode = 2001001
-        versionName = "2.1.1"
+        versionCode = 2002000
+        versionName = "2.2.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
